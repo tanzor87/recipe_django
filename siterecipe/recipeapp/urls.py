29698b5b50d365443d407662ae14253394904a04
andrey <tanzor87@mail.ru>
2024-12-30 00:34:08 +0700

    add static files

--- siterecipe/recipeapp/urls.py
+++ siterecipe/recipeapp/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path, include
-from .views import RecipeIndexView, AboutView, addrecipe, login
+from .views import (
+    RecipeIndexView,
+    AboutView,
+    addrecipe,
+    login,
+    show_detail,
+)
 
 urlpatterns = [
     path('', RecipeIndexView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
-    path('addrecipe', addrecipe, name='add_recipe'),
-    path('login', login, name='login'),
+    path('addrecipe/', addrecipe, name='add_recipe'),
+    path('login/', login, name='login'),
+    path('detail/<int:detail_id>/', show_detail, name='detail')
 ]
