400a1c3e8a1312497fa0d2066147516c6af7ba6b
andrey <tanzor87@mail.ru>
2025-01-12 23:24:00 +0700

    create recipe_01

--- siterecipe/recipeapp/models.py
+++ siterecipe/recipeapp/models.py
@@ -1,11 +1,21 @@
+from math import trunc
+
 from django.db import models
 from django.urls import reverse
+
+
+class RecipeBase(models.Model):
+    recipe_title = models.CharField(max_length=255)
+    short_description = models.TextField(blank=True)
+    cooking_description = models.TextField(blank=True)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT)
 
 
 class Ingredients(models.Model):
     ingredient_name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
-
 
     class Meta:
         ordering = ['ingredient_name']
@@ -15,3 +25,14 @@ class Ingredients(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'detail_slug': self.slug})
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=100, db_index=True)
+    category_slug = models.SlugField(max_length=100, blank=True, db_index=True, default='')
+
+    class Meta:
+        ordering = ['category_name']
+
+    def __str__(self):
+        return self.category_name
--- siterecipe/recipeapp/urls.py
+++ siterecipe/recipeapp/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
     path('addrecipe/', addrecipe, name='add_recipe'),
     path('login/', login, name='login'),
-    # path('detail/<int:detail_id>/', show_detail, name='detail'),
-    path('detail/<slug:detail_slug>/', show_detail, name='detail'),
+    path('detail/<int:detail_id>/', show_detail, name='detail'),
+    # path('detail/<slug:detail_slug>/', show_detail, name='detail'),
     path('category/<int:category_id>/', show_category, name='category'),
 ]
