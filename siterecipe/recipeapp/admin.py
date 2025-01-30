41574b1d06850e07216685601bbba533ed4654ff
Andrey <rsdak@ksc.krasn.ru>
2025-01-30 11:49:58 +0700

    addition photos to site

--- siterecipe/recipeapp/admin.py
+++ siterecipe/recipeapp/admin.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
+
 from .models import RecipeBase, Category
 
 
 @admin.register(RecipeBase)
 class RecipeAdmin(admin.ModelAdmin):
-    # fields = ['recipe_title', 'short_description', 'cooking_description', 'category']
+    fields = ['recipe_title', 'photos', 'recipe_photo',  'short_description', 'cooking_description', 'category']
     # filter_horizontal = ['recipe_ingredients']
-    list_display = ('id', 'recipe_title', 'short_description', 'time_update')
+    readonly_fields = ['recipe_photo']
+    list_display = ('id', 'recipe_title', 'recipe_photo', 'short_description', 'time_update')
     list_display_links = ('id', 'recipe_title')
     ordering = ['time_update', 'recipe_title']
     list_per_page = 10
     search_fields = ['recipe_title']
     list_filter = ['category__category_name']
+
+
+    @admin.display(description='Изображение', ordering='short_description')
+    def recipe_photo(self, recipe: RecipeBase):
+        if recipe.photos:
+            return mark_safe(f"<img src='{recipe.photos.url}' width=50>")
+        return 'Без фото'
 
 
 @admin.register(Category)
--- siterecipe/siterecipe/urls.py
+++ siterecipe/siterecipe/urls.py
@@ -15,15 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import handler404
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
 from recipeapp.views import page_not_found
 
+from siterecipe import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipeapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
 
